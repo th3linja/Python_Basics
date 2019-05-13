02300c232de0e7f69ed9cfbf52c82559ecae35b8
th3linja <th3linja@yahoo.com>
2019-05-13 13:47:01 -0700

    files uploaded

---
+++ avg_trip_time.py
@@ -0,0 +1,23 @@
+# welcomes user to the program
+print("Welcome to the Trip Time Calculator")
+print()
+
+# ask user for miles to be traveled and average mph
+travel = input("Enter the amount of miles to be traveled: ")
+while travel.isdigit() is False or int(travel)<0:
+    travel = input("Please enter a valid input: ")
+mph = input("Enter the average mph of the trip: ")
+while mph.isdigit() is False or int(mph) < 0:
+    mph = input("Please enter a valid input: ")
+print()
+
+
+# calculate based on given information
+hours = int(travel)//int(mph)
+minutes = int(travel) - hours*int(mph)
+
+# return calculations
+print("Estimated travel time")
+print("Hours: " + str(hours))
+print("Minutes: " + str(minutes))
+
